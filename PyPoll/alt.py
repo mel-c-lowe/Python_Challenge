0cd3bc622646b9774b6326888a1d90a3a24c011a
Melissa Lowe <melissa@Melissas-MacBook-Pro.local>
2021-01-19 20:24:20 -0600

    Many thanks to Terra and Beau for meeting and talking through some of the elements.  Hard coding of candidate names is resolved (presentation of data not quite resolved), and more clarity in PyBank generally.

--- PyPoll/alt.py
+++ PyPoll/alt.py
@@ -20,50 +20,74 @@ with open(election_csv, 'r') as election_data:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    vote_khan = 0
-    vote_correy = 0
-    vote_li = 0
-    vote_otooley = 0
+    #stuff below thanks to beau
+
+    total_votes = 0
+    winner = 0
+    candidate_dict = {}
+    percent_dict = {}
+
+    for row in csvreader:
+
+        candidate = row[2]
+        total_votes += 1
+        votes = {}
+
+        if candidate not in candidate_dict:
+            candidate_dict[candidate] = 1
+
+        else:
+            candidate_dict[candidate] += 1
+
+    print(candidate_dict)
+
+  
+
+
+
+
+
+
+
+    # vote_khan = 0
+    # vote_correy = 0
+    # vote_li = 0
+    # vote_otooley = 0
     
-    # could I do a while loop starting with an empty variable 'active_candidate'?
-    # candidate = []
-    #while candidate == "this is where I don't know how to execute"
-        #put active candidate into dictionary as a value to key of Candidate
-        #if candidate == the next row, then
-            #add to the counter for that candidate
-            #append candidate name to list of candidates?
-            #use list to create dictionary
-                #maybe no?
-            #produce results based on dictionary???
+    # # could I do a while loop starting with an empty variable 'active_candidate'?
+    # # candidate = []
+    # #while candidate == "this is where I don't know how to execute"
+    #     #put active candidate into dictionary as a value to key of Candidate
+    #     #if candidate == the next row, then
+    #         #add to the counter for that candidate
+    #         #append candidate name to list of candidates?
+    #         #use list to create dictionary
+    #             #maybe no?
+    #         #produce results based on dictionary???
 
 
-    for candidate in csvreader:
-        if candidate[2] == 'Khan':
-            vote_khan += 1
+    # for candidate in csvreader:
+    #     if candidate[2] == 'Khan':
+    #         vote_khan += 1
 
-        elif candidate[2] == 'Correy':
-            vote_correy += 1
+    #     elif candidate[2] == 'Correy':
+    #         vote_correy += 1
 
-        elif candidate[2] == 'Li':
-            vote_li += 1
+    #     elif candidate[2] == 'Li':
+    #         vote_li += 1
 
-        elif candidate[2] == "O'Tooley":
-            vote_otooley += 1
+    #     elif candidate[2] == "O'Tooley":
+    #         vote_otooley += 1
 
-    #This works below the counter but not above. Weird.
-    # total_votes = len(list(csvreader)
+    # #This works below the counter but not above. Weird.
+    # # total_votes = len(list(csvreader)
 
     
-    print("The vote count for Khan is")
-    print(vote_khan) 
+    # print("The vote count for Khan is")
+    # print(vote_khan) 
     
 
 
 
-    # print("The vote count for Correy is")
-    # print(vote_correy)
-    # print("The vote count for Li is") 
-    # print(vote_li)
-    # print("The vote count for O'Tooley is") 
-    # print(vote_otooley)
+   
 
